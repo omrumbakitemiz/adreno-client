d7fa7f4f428191810f0a8d3fdd8ada0184f6ca79
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-03-20 17:27:23 +0300

    circli ci config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

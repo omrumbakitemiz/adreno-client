a0f95061639c169c6851ed9cceddbbff335ed1de
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-03-20 16:43:51 +0300

    sonarqube, eslint configs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'adreno-client'
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>adreno</h1>
+        <h1>{this.state.title}</h1>
       </div>
     );
   }
